a3275b3d5e1f6d55d9b5fd9b5fccdd307375b92b
jiokeokwuosa <okwuosachijioke@gmail.com>
2021-02-02 16:24:13 +0100

    chore: adjust unit tes again

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -19,67 +19,65 @@ class AuthController {
    * @memberof AuthController
    * @returns {JSON} - A JSON success response.
    */
-  static async signUp(req, res) {
-    // try {
-    //   const {
-    //     fullName, password, email, role,
-    //   } = req.body;
+  // static async signUp(req, res) {
+  //   // try {
+  //   //   const {
+  //   //     fullName, password, email, role,
+  //   //   } = req.body;
 
-    //   const encryptpassword = await Helper.encrptPassword(password);
+  //   //   const encryptpassword = await Helper.encrptPassword(password);
 
-    //   const newUser = {
-    //     fullName,
-    //     password: encryptpassword,
-    //     email,
-    //     role,
-    //   };
+  //   //   const newUser = {
+  //   //     fullName,
+  //   //     password: encryptpassword,
+  //   //     email,
+  //   //     role,
+  //   //   };
 
-    //   const result = await Auth.create({ ...newUser });
+  //   //   const result = await Auth.create({ ...newUser });
 
-    //   const enrolledCourse = await EnrolledCourse.create({
-    //     userId: result._id,
-    //     courseId: req.body.courseId,
-    //   });
+  //   //   const enrolledCourse = await EnrolledCourse.create({
+  //   //     userId: result._id,
+  //   //     courseId: req.body.courseId,
+  //   //   });
 
-    //   // if role is a teacher && there are className and courseId in body
-    //   // create class with the info
-    //   if (role === '5fc8cc978e28fa50986ecac9') {
-    //     let classCode = await Helper.generateCode(8);
-    //     const existingClassCode = await ClassModel.findOne({ classCode });
-    //     if (existingClassCode) {
-    //       classCode = await Helper.generateCode(8);
-    //     }
-    //     const newClass = await ClassModel.create({
-    //       userId: result._id,
-    //       name: req.body.className,
-    //       courseId: req.body.courseId,
-    //       classCode,
-    //     });
+  //   //   // if role is a teacher && there are className and courseId in body
+  //   //   // create class with the info
+  //   //   if (role === '5fc8cc978e28fa50986ecac9') {
+  //   //     let classCode = await Helper.generateCode(8);
+  //   //     const existingClassCode = await ClassModel.findOne({ classCode });
+  //   //     if (existingClassCode) {
+  //   //       classCode = await Helper.generateCode(8);
+  //   //     }
+  //   //     const newClass = await ClassModel.create({
+  //   //       userId: result._id,
+  //   //       name: req.body.className,
+  //   //       courseId: req.body.courseId,
+  //   //       classCode,
+  //   //     });
 
-    //     await enrolledCourse.update({ classId: newClass._id }, { new: true });
-    //     await enrolledCourse.save();
-    //   }
+  //   //     await enrolledCourse.update({ classId: newClass._id }, { new: true });
+  //   //     await enrolledCourse.save();
+  //   //   }
 
-    //   const user = await AuthServices.emailExist(email, res);
-    //   const token = await Helper.generateToken(result._id, role, fullName);
+  //   //   const user = await AuthServices.emailExist(email, res);
+  //   //   const token = await Helper.generateToken(result._id, role, fullName);
 
-    //   // const message = `Please verify your email address to complete your Afrilearn Account.<br/>Click the link https://www.myafrilearn.com/?token=${token}`;
+  //   //   // const message = `Please verify your email address to complete your Afrilearn Account.<br/>Click the link https://www.myafrilearn.com/?token=${token}`;
 
-    //   return res.status(201).json({
-    //     status: 'success',
-    //     data: {
-    //       token,
-    //       user,
-    //     },
-    //   });
-    // } catch (err) {
-    //   return res.status(500).json({
-    //     status: '500 Internal server error',
-    //     error: 'Error creating new user',
-    //   });
-    // }
-  }
-
-
+  //   //   return res.status(201).json({
+  //   //     status: 'success',
+  //   //     data: {
+  //   //       token,
+  //   //       user,
+  //   //     },
+  //   //   });
+  //   // } catch (err) {
+  //   //   return res.status(500).json({
+  //   //     status: '500 Internal server error',
+  //   //     error: 'Error creating new user',
+  //   //   });
+  //   // }
+  // }
 }
 export default AuthController;
