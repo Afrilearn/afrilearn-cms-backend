568338d080856901938bdbed3d0bdadb3234161d
VictorOnwukwe <victoronwukwe25@gmail.com>
2021-02-08 14:56:05 +0100

    ft(timestamp):Add createdAt and updatedAt

--- src/db/models/cmsUsers.model.js
+++ src/db/models/cmsUsers.model.js
@@ -26,6 +26,14 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: true,
     },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
   {
     toJSON: { virtuals: true },
