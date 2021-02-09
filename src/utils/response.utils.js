41a9603cd10b557287aad19306a224513aa734a8
VictorOnwukwe <victoronwukwe25@gmail.com>
2021-02-09 16:50:15 +0100

    Feature: Implement cms user signup

--- src/utils/response.utils.js
+++ src/utils/response.utils.js
@@ -3,83 +3,86 @@
  */
 export default class {
   /**
-     * Defines the specification for the "unauthorized error" response cases
-     * @param {Object} res
-     * @param {Object} error
-     * @param {number} code
-     * @returns {Object} response
-     */
+   * Defines the specification for the "unauthorized error" response cases
+   * @param {Object} res
+   * @param {Object} error
+   * @param {number} code
+   * @returns {Object} response
+   */
   static UnauthorizedError(res, error, code) {
     return res.status(code || 401).json({
-      status: 'error',
+      status: "error",
       error,
     });
   }
 
   /**
-     * Defines the specification for the "success" response cases
-     * @param {Object} res
-     * @param {Object} data
-     * @param {number} code
-     * @returns {ServerResponse} response
-     */
+   * Defines the specification for the "success" response cases
+   * @param {Object} res
+   * @param {Object} data
+   * @param {number} code
+   * @returns {ServerResponse} response
+   */
   static Success(res, data, code = 200) {
     return res.status(code).json({
-      status: 'success',
+      status: "success",
       data,
     });
   }
 
+  /**
+   * @returns {JSON} - JSON error object with 500 status
+   * @param {*} res - Response Object
+   * @param {*} message - error message
+   * @param {*} err - thrown error
+   */
   static InternalServerError(res, message, err) {
     return res.status(500).json({
-      status: '500 Internal server error',
+      status: "500 Internal server error",
       error: message,
-      errors: err || [message],
+      errors: err,
     });
   }
 
   /**
-     * @returns {JSON} - JSON error object with 400 status
-     * @param {*} res - Response Object
-     * @param {*} errs - thrown errors
-     * @param {*} message - error message
-     */
-    static InvalidDataRequest(
-      res,
-      errs,
-      message = 'Request contains invalid data',
-    ) {
-      return res.status(400).json({
-        status: '400 Invalid Request',
-        error: message,
-        errors: errs,
-      });
-    }
-  
-    /**
-       * @returns {JSON} - JSON error object with status 401
-       * @param {*} res - Response Object
-       * @param {*} message - error message
-       */
-    static Unauthorised(res, message = 'Not authorized to access data') {
-      return res.status(401).json({
-        status: '401 Unauthorized',
-        error: message,
-      });
-    }
-  
-    /**
-       * @returns {JSON} - JSON error object with status 409
-       * @param {*} res - Response Object
-       * @param {*} message - error message
-       */
-    static ConflictingRequest(
-      res,
-      message,
-    ) {
-      return res.status(409).json({
-        status: '409 Conflicting Request',
-        error: message,
-      });
-    }
+   * @returns {JSON} - JSON error object with 400 status
+   * @param {*} res - Response Object
+   * @param {*} errs - thrown errors
+   * @param {*} message - error message
+   */
+  static InvalidDataRequest(
+    res,
+    errs,
+    message = "Request contains invalid data"
+  ) {
+    return res.status(400).json({
+      status: "400 Invalid Request",
+      error: message,
+      errors: errs,
+    });
+  }
+
+  /**
+   * @returns {JSON} - JSON error object with status 401
+   * @param {*} res - Response Object
+   * @param {*} message - error message
+   */
+  static Unauthorised(res, message = "Not authorized to access data") {
+    return res.status(401).json({
+      status: "401 Unauthorized",
+      error: message,
+    });
+  }
+
+  /**
+   * @returns {JSON} - JSON error object with status 409
+   * @param {*} res - Response Object
+   * @param {*} message - error message
+   */
+  static ConflictingRequest(res, message) {
+    return res.status(409).json({
+      status: "409 Conflicting Request",
+      error: message,
+    });
+  }
 }
