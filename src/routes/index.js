5abd76ec311dbcd984ad46f42dcb8fc2d2c8dced
damlois <lois0168@gmail.com>
2021-02-11 12:01:59 +0100

    minor update

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import authRouter from './auth.route';
 import userRouter from './users.route';
 import coursesRouter from './courses.route';
-import mainSubjectsRouter from './major-subject.route';
+import mainSubjectsRouter from './mainSubjects.route';
 import termRouter from './terms.route';
 import courseCategoryRouter from './courseCategories.route';
 
